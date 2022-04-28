db15eaa3a7db6f60166459a4b35b018517c1d4b1
Gonzalo Huerta <gonzaloahuerta@gmail.com>
2022-04-28 12:52:27 -0300

    Subo enunciado desafío 3

--- 03/server.js
+++ 03/server.js
@@ -1,11 +1,9 @@
-import express from 'express'
+import express from 'express';
+const app = express();
 import Contenedor from './contenedor.js';
 
 const prueba = new Contenedor('productos.txt');
 
-//console.log(await prueba.getAll());
-
-const app = express();
 const PORT = 8080;
 app.listen(8080);
 console.log("Servidor http escuchando en el puerto", PORT);
@@ -14,6 +12,9 @@ let productos = await prueba.getAll();
 
 let lengthProductos = productos.length-1;
 
+app.get('/', (req, res)=>{
+    res.send(`<h1>Inicio</h1><h2><a href="/productos">Ver todos los productos</a><br/><br/><a href="productoRandom">Ver un producto aleatorio</a></h2>`)
+})
 
 app.get('/productos', (req, res)=>{
     res.send(`<h1 style='color: #2b2b2b;'>Todos los productos</h1> <h3>${JSON.stringify(productos)}</h3>`)
